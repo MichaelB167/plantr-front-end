c0997d3df87902979e7392b6ba4ad9be0a3bd7c3
MichaelB167 <michael.m.bates@gmail.com>
2016-06-05 17:53:19 -0400

    Add nested routes for plant types and harvest

--- app/router.js
+++ app/router.js
@@ -11,6 +11,15 @@ Router.map(function () {
   this.route('change-password');
   this.route('users');
   this.route('plants');
+  this.route('plant', function() {
+    this.route('all');
+    this.route('annuals');
+    this.route('perennials');
+    this.route('herbs');
+    this.route('vegetables');
+    this.route('fruits');
+    this.route('harvest');
+  });
 });
 
 export default Router;
