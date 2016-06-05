86d999560e3cc9fb785f63402a6de5c3e44aa8d2
MichaelB167 <michael.m.bates@gmail.com>
2016-06-05 14:25:46 -0400

    Change instances of ga-wdi-boston.ember-auth to plantr

--- tests/unit/initializers/text-field-test.js
+++ tests/unit/initializers/text-field-test.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import TextFieldInitializer from 'ga-wdi-boston.ember-auth/initializers/text-field';
+import TextFieldInitializer from 'plantr/initializers/text-field';
 import { module, test } from 'qunit';
 
 let application;
