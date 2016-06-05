b5e540c04e88fa0a14b463f100370027cdb72b12
MichaelB167 <michael.m.bates@gmail.com>
2016-06-05 18:45:19 -0400

    scaffold routes and templates for plant categories

---
+++ tests/unit/plant/harvest/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:plant/harvest', 'Unit | Route | plant/harvest', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
