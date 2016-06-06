c23cc20d14eeb1a378b35e060c09fc61a44430d6
MichaelB167 <michael.m.bates@gmail.com>
2016-06-06 15:41:19 -0400

    Add seemingly working delete action for annuals

--- app/components/annual-component/component.js
+++ app/components/annual-component/component.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    destroyPlant: function() {
+      this.sendAction('routeDestroyPlant', this.get('plant'));
+    }
+  }
 });
