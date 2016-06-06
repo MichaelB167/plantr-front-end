978b41ac5d872c298696a40db69cb46191d8d612
MichaelB167 <michael.m.bates@gmail.com>
2016-06-06 12:18:54 -0400

    Fix return of model hook to store instead of plant

--- app/plant/annual/route.js
+++ app/plant/annual/route.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    return {
+      plant: this.store.findAll('plant')
+    };
+  },
+  actions: {
+    createPlant: function(data) {
+      this.store.createRecord('plant', data).save();
+    }
+  }
 });
