18add12ae32051491961aa029bf001bd33ae1bfa
MichaelB167 <michael.m.bates@gmail.com>
2016-06-05 20:39:23 -0400

    scaffold forms for creating a plant

---
+++ tests/unit/components/plant-form/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:components/plant-form', 'Unit | Route | components/plant form', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
