af85caa133bcd938982f6f6bfde03b708f8cb414
MichaelB167 <michael.m.bates@gmail.com>
2016-06-06 16:41:27 -0400

    Add update plant action to annual component Create toggle property to switch between template and edit

--- app/components/annual-component/component.js
+++ app/components/annual-component/component.js
@@ -1,9 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  canUpdate: false,
   actions: {
     destroyPlant: function() {
       this.sendAction('routeDestroyPlant', this.get('plant'));
+    },
+    updatePlant: function() {
+      this.set('plant.category', Ember.$('select').val());
+      this.sendAction('routeUpdatePlant', this.get('plant'));
+      this.set('canUpdate', false);
+    },
+    edit: function(){
+      this.toggleProperty('canUpdate');
     }
   }
 });
