7daa0647712253c2e2604339cbb6d3980ca34a6b
MichaelB167 <michael.m.bates@gmail.com>
2016-06-06 21:48:13 -0400

    Add route and template to vegetable from annual

--- app/components/vegetable-component/component.js
+++ app/components/vegetable-component/component.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  canUpdate: false,
+  hasHarvested: false,
+  actions: {
+    destroyPlant: function() {
+      this.sendAction('routeDestroyPlant', this.get('plant'));
+    },
+    updatePlant: function() {
+      this.set('plant.category', Ember.$('select').val());
+      this.sendAction('routeUpdatePlant', this.get('plant'));
+      this.set('canUpdate', false);
+      this.set('hasHarvested', false);
+    },
+    edit: function(){
+      this.toggleProperty('canUpdate');
+    },
+    harvested: function(){
+      this.toggleProperty('hasHarvested');
+    },
+    harvestPlant: function() {
+      this.set('plant.harvest', 'true');
+      this.sendAction('routeUpdatePlant', this.get('plant'));
+      this.set('canUpdate', false);
+      this.set('hasHarvested', false);
+    }
+  }
 });
