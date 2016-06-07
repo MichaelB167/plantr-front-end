21c20175afdfcdd3b32f99d01e406669efae707f
MichaelB167 <michael.m.bates@gmail.com>
2016-06-06 21:49:20 -0400

    Add route and template to fruit component from annual component

--- app/plant/fruit/route.js
+++ app/plant/fruit/route.js
@@ -9,6 +9,12 @@ export default Ember.Route.extend({
   actions: {
     createPlant: function(data) {
       this.store.createRecord('plant', data).save();
+    },
+    updatePlant: function(plant) {
+      plant.save();
+    },
+    destroyPlant: function(plant) {
+      plant.destroyRecord();
     }
   }
 });
