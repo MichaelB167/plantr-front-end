83996adc65ec8cddafc43a1b435b3a57fed145de
MichaelB167 <michael.m.bates@gmail.com>
2016-06-07 15:50:36 -0400

    Remove all harvest options from annual component Because annuals are inedible

--- app/components/annual-component/component.js
+++ app/components/annual-component/component.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   canUpdate: false,
-  hasHarvested: false,
   actions: {
     destroyPlant: function() {
       this.sendAction('routeDestroyPlant', this.get('plant'));
@@ -16,14 +15,5 @@ export default Ember.Component.extend({
     edit: function(){
       this.toggleProperty('canUpdate');
     },
-    harvested: function(){
-      this.toggleProperty('hasHarvested');
-    },
-    harvestPlant: function() {
-      this.set('plant.harvest', 'true');
-      this.sendAction('routeUpdatePlant', this.get('plant'));
-      this.set('canUpdate', false);
-      this.set('hasHarvested', false);
-    }
   }
 });
