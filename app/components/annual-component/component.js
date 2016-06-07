f6ae082f7dfe57665028d43dec1913365a535fb3
MichaelB167 <michael.m.bates@gmail.com>
2016-06-06 21:27:57 -0400

    fix harvestPlant action to set harvest as 'true'

--- app/components/annual-component/component.js
+++ app/components/annual-component/component.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   canUpdate: false,
+  hasHarvested: false,
   actions: {
     destroyPlant: function() {
       this.sendAction('routeDestroyPlant', this.get('plant'));
@@ -10,14 +11,19 @@ export default Ember.Component.extend({
       this.set('plant.category', Ember.$('select').val());
       this.sendAction('routeUpdatePlant', this.get('plant'));
       this.set('canUpdate', false);
+      this.set('hasHarvested', false);
     },
     edit: function(){
       this.toggleProperty('canUpdate');
     },
-    harvest: function() {
+    harvested: function(){
+      this.toggleProperty('hasHarvested')
+    },
+    harvestPlant: function() {
       this.set('plant.harvest', 'true');
       this.sendAction('routeUpdatePlant', this.get('plant'));
       this.set('canUpdate', false);
+      this.set('hasHarvested', false);
     }
   }
 });
