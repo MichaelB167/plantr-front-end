1dc3ee1d5c6c7f07dbd7b48ea11b146a2af8a785
MichaelB167 <michael.m.bates@gmail.com>
2016-06-07 09:11:01 -0400

    Create harvest component from annual component Remove unnecessary harvest update actions

---
+++ app/components/harvest-component/component.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  canUpdate: false,
+  actions: {
+    destroyPlant: function() {
+      this.sendAction('routeDestroyPlant', this.get('plant'));
+    },
+    updatePlant: function() {
+      this.set('plant.category', Ember.$('select').val());
+      this.sendAction('routeUpdatePlant', this.get('plant'));
+      this.set('canUpdate', false);
+      this.set('hasHarvested', false);
+    },
+    edit: function(){
+      this.toggleProperty('canUpdate');
+    },
+  }
+});
