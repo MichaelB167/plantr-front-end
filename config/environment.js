1166fae16bc900251bdb1763dc6bb4c2694f1b0e
MichaelB167 <michael.m.bates@gmail.com>
2016-06-07 22:03:26 -0400

    Add production parameters for gh-pages push

--- config/environment.js
+++ config/environment.js
@@ -40,8 +40,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
-  }
+  ENV.baseURL = '/platr-front-end';
+  ENV.locationType = 'hash';
+}
 
   return ENV;
 };
